934aa8cfd5516ee1d5b800faee2059fdb4e8a5de
Dave Lucia <davelucianyc@gmail.com>
2019-10-22 13:33:16 -0400

    Add some TODOs

--- native/pig_latin_nif/src/lib.rs
+++ native/pig_latin_nif/src/lib.rs
@@ -12,6 +12,8 @@ rustler::rustler_export_nifs! {
 fn translate_to_pig_latin(mut word: String) -> String {
     let mut tail = word.split_off(1);
 
+    // TODO handle single letter words
+
     tail.push_str(" ");
     tail.push_str(&word);
     tail.push_str("ay");
@@ -21,6 +23,9 @@ fn translate_to_pig_latin(mut word: String) -> String {
 
 fn translate<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
     let word: String = args[0].decode()?;
+
+    // TODO handle sentences
+    // TODO handle grammar
 
     Ok((ok(), translate_to_pig_latin(word)).encode(env))
 }
